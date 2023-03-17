84edc766b6b19b3ef744b976a419f98230a28e1e
DucPhatNg9a5 <56929794+DucPhatNg9a5@users.noreply.github.com>
2023-03-17 17:46:48 -0600

    Add project files.

---
+++ Data/FlightObj.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Traveless.Data;
+
+public class FlightObj
+{
+	public string FlightCode { get; set; }
+	public string DepartureAirport { get; set; }
+	public string DestinationAirport { get; set; }
+	public string FlyingDay { get; set; }
+	public string FlyingTime { get; set; }
+	public int Seats { get; set; }
+	public double Cost { get; set; }
+	public string airline { get; set; }
+	public FlightObj(String flightCode,String airline, String departureAirport, string destinationAirport, string flyingDay, string flyingTime, int seats, double cost)
+	{
+		this.FlightCode= flightCode;
+		switch (airline)
+		{
+			case "AS":
+				airline = "AirShaker";
+				break;
+			case "AL":
+				airline = "Always Late Airlines";
+				break;
+			case "CA":
+				airline = "Conned Air";
+				break;
+			case "EC":
+				airline = "Error Canada";
+				break;
+			case "GA":
+				airline = "Gypped Air";
+				break;
+			case "MA":
+				airline = "Mediocre Airlines";
+				break;
+			case "OA":
+				airline = "Otto Airlines";
+				break;
+			case "SL":
+				airline = "Scare Airlines";
+				break;
+			case "ST":
+				airline = "ScareTransat";
+				break;
+			case "SW":
+				airline = "Sprawl Airways";
+				break;
+			case "TB":
+				airline = "Try a Bus Airways";
+				break;
+			case "VA":
+				airline = "Vertical Airways";
+				break;
+		}
+		this.airline = airline;
+		this.DepartureAirport= departureAirport;	
+		this.DestinationAirport= destinationAirport;
+		this.FlyingDay= flyingDay;
+		this.FlyingTime= flyingTime;
+		this.Seats= seats;
+		this.Cost= cost;
+	}
+	public override string ToString()
+	{
+		return($"{FlightCode},{DepartureAirport},{DestinationAirport},{FlyingDay},{FlyingTime},{Seats},{Cost},{airline}");
+	}
+}
---
+++ Data/ReservationObj.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveless.Data
+{
+	public class ReservationObj
+	{
+		public string ReservCode { get; set; }
+		public FlightObj Flight { get; set; }
+
+		public string Name { get; set; }
+
+		public string Nationality { get; set; }
+
+		public string Status { get; set; } = "Active";
+		public ReservationObj(string reservCode, FlightObj flight, string name, string nationality)
+		{
+			this.ReservCode = reservCode;
+			this.Flight = flight;
+			this.Name = name;
+			this.Nationality = nationality;
+		}
+		public override string ToString()
+		{
+			return ($"{ReservCode},{Flight.FlightCode},{Flight.airline},{Flight.Cost},{Name},{Nationality},{Status}");
+		}
+	}
+}
